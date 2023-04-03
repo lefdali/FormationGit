69dbbd345b5b22f65be88a9299e49e1d9610ae89
ELHASSANIsaad <eckolima20@gmail.com>
2023-04-03 12:26:23 +0000

    new test version 01

--- Formation/Formation/Program.cs
+++ Formation/Formation/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Formation;
 
-Console.WriteLine("Hello, World! elhassani version ");
+Console.WriteLine("Hello, World!  version 01");
 
 
 Console.WriteLine("Hello, Sqli, to dev brach");
