ba8c634373d7008758515a1301d3468eee49ea3d
mouadKharchich <mouaadkharchich19@gmail.com>
2023-04-03 11:26:19 +0000

    add formation and student class

---
+++ Formation/Formation/Formations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formation
+{
+    public class Formations
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Student> Students { get; set; } = new List<Student>();
+    }
+}
--- Formation/Formation/Program.cs
+++ Formation/Formation/Program.cs
@@ -1,5 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Formation;
+
 Console.WriteLine("Hello, World!  ");
+
+Formations git = new Formations();
+
+git.Name = "git";
+git.Id = 1; 
+
+List<Student> students = new List<Student>()
+{
+    new Student() { Id = 1,Name="Mouaad"}
+};   
+
+git.Students = students;
 
 //branche blefdali22
 
