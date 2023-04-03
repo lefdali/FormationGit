e0822a43c56fe0dc9a45295aea4d6c9b9cfba7e7
younes888796 <127496533+younes888796@users.noreply.github.com>
2023-04-03 13:05:22 +0000

    okkk pour v1

--- Formation/Formation/Program.cs
+++ Formation/Formation/Program.cs
@@ -12,6 +12,7 @@ Console.WriteLine("Hello, world, Younes version ");
 Console.WriteLine("Hello, Sqli, to dev brach");
 Console.WriteLine("Hello, Sqli, saad");
 Console.WriteLine("Hello, Sqli, Younesse");
+Console.WriteLine("Hello, Sqli, Younesse 011");
 List<Restaurant> restaurants = new List<Restaurant>();
 restaurants.Add(new Restaurant() { Id = 1, Name = "name1" });
 Console.WriteLine(restaurants.Count);
