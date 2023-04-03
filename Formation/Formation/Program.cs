de38414f106957b6b2dd0af696ceef13a3d9b4f4
ELHASSANIsaad <eckolima20@gmail.com>
2023-04-03 12:21:34 +0000

    elhassani commit

--- Formation/Formation/Program.cs
+++ Formation/Formation/Program.cs
@@ -1,22 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Formation;
 
-Console.WriteLine("Hello, World!  ");
-
-Formations git = new Formations();
-
-git.Name = "git";
-git.Id = 1; 
-
-List<Student> students = new List<Student>()
-{
-    new Student() { Id = 1,Name="Mouaad"}
-};   
-
-git.Students = students;
+Console.WriteLine("Hello, World! elhassani version ");
 
 
-console.WriteLine("mkharchich changes")
+Console.WriteLine("Hello, Sqli, to dev brach");
+
 
 //branche blefdali22
 
