9e1765ad66e62220b7063afe0188448ca3aabceb
mouadKharchich <mouaadkharchich19@gmail.com>
2023-04-03 12:42:26 +0000

    ooooo

--- Formation/Formation/Program.cs
+++ Formation/Formation/Program.cs
@@ -6,6 +6,10 @@ Console.WriteLine("Hello, World!  version 01");
 Console.WriteLine("Hello, world, Younes version ");
 
 Console.WriteLine("Hello, Sqli, to dev brach")
+
+
+
+console.WriteLine("mkharchich changes")
 //branche blefdali22
 //mkharchich change
 
