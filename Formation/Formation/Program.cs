22de5bf9411c29c8baa4d94fe498dfd8dc48debb
taha jaaouan <tjaaouantaha@gmail.com>
2023-04-03 12:36:26 +0000

    changing program file

--- Formation/Formation/Program.cs
+++ Formation/Formation/Program.cs
@@ -7,7 +7,9 @@ Console.WriteLine("Hello, world, Younes version ");
 
 Console.WriteLine("Hello, Sqli, to dev brach");
 
-
+List<Restaurant> restaurants = new List<Restaurant>();
+restaurants.Add(new Restaurant() { Id = 1, Name = "name1" });
+Console.WriteLine(restaurants.Count);
 //branche blefdali22
 
 
