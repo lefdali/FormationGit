86b4f8aceb1eb5af3a5816fbfe8c8a2b7dd7f2c9
mouadKharchich <mouaadkharchich19@gmail.com>
2023-04-03 12:41:13 +0000

    ooo

--- Formation/Formation/Program.cs
+++ Formation/Formation/Program.cs
@@ -5,19 +5,7 @@ Console.WriteLine("Hello, World!  version 01");
 
 Console.WriteLine("Hello, world, Younes version ");
 
-git.Name = "git";
-git.Id = 1; 
-
-List<Student> students = new List<Student>()
-{
-    new Student() { Id = 1,Name="Mouaad"}
-};   
-
-git.Students = students;
-
-
-console.WriteLine("mkharchich changes")
-
+Console.WriteLine("Hello, Sqli, to dev brach")
 //branche blefdali22
 //mkharchich change
 
