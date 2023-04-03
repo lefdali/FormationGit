546536da5461b233e224c124d34b007ce10331d2
waramdani <waramdani@RABCSDDWT2>
2023-04-03 12:36:00 +0000

    updated commit

--- Formation/Formation/Program.cs
+++ Formation/Formation/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-<<<<<<< HEAD
+//using Formation;
+
 Console.WriteLine("Hello, World!  ");
-Console.WriteLine("Hello, it's my commit here !  ");
-=======
-using Formation;
+
+Console.WriteLine("Hello, it's walid commit here !  ");
 
 Console.WriteLine("Hello, World!  version 01");
 
@@ -11,7 +11,6 @@ Console.WriteLine("Hello, world, Younes version ");
 
 Console.WriteLine("Hello, Sqli, to dev brach");
 
->>>>>>> ec1440c702ee022e51501c498098f2c87d696528
 
 //branche blefdali22
 
